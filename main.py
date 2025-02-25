a58ef67bb0fe355f53d2918388a30ce17b75751d
alexchtst-01 <acinatra@gmail.com>
2025-02-25 23:46:55 +0700

    feat: minor fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
@@ -19,7 +19,9 @@ with app.app_context():
     db.create_all()
     
 app.register_blueprint(api)
-
+@app.route('/')
+def index():
+    return "Selamat datang pengunjung, awokwok"
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=5000)
